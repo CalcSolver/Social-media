125180bab98f2b3fe379e95c5c15260c1619d9dd
Calculator <hjass2865@gmail.com>
2026-06-29 18:27:20 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+window.signup = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password
+  });
+
+  if (error) {
+    alert("Signup error: " + error.message);
+  } else {
+    alert("Signup successful! Now click Login.");
+  }
+};
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password
+  });
+
+  if (error) {
+    alert("Login error: " + error.message);
+  } else {
+    alert("Login successful!");
+  }
+};
