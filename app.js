f1aad8bddef605b23b43ad90621f896cbd6ff0d8
Calculator <hjass2865@gmail.com>
2026-06-29 18:31:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("JS is working!");
 window.signup = async function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
